130647361a0ca7205e2098d295ba5e86e59f9f1a
Santa21322 <getnetsintayehu05@gmail.com>
2024-11-16 22:11:27 +0300

    adding featured section and car images to image folder

--- script.js
+++ script.js
@@ -72,3 +72,30 @@ document.querySelector('.home').onmouseleave = () => {
         },
       },
     });
+
+     var swiper = new Swiper(".featured-slider", {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      loop: true,
+      grapCursor: true,
+      centeredSlides: true,
+      autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+      },
+    });
